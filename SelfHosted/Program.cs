1a8aa4ba4ece96b4486e88b32f43fa18c3ec5c3c
Thomas Bandixen <thomas@bandixen.ch>
2012-10-25 20:21:59 +0200

    initial commit

---
+++ SelfHosted/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace SelfHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new HttpSelfHostConfiguration("http://localhost:1337");
+
+            cfg.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            cfg.Routes.MapHttpRoute(
+    "Default", "{res}",
+    new { controller = "Home", res = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(cfg))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine(cfg.BaseAddress);
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
