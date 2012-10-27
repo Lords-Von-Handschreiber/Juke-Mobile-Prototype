6a286440c2aeedaf7e376b8e5350b54cf1af8aeb
peac3maker <peac3maker@gmx.ch>
2012-10-27 11:37:40 +0200

    Added File extensions.

--- SelfHosted/FileUploadController.cs
+++ SelfHosted/FileUploadController.cs
@@ -9,6 +9,7 @@ using System.Web.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Formatting;
 using System.IO;
+using System.Collections.ObjectModel;
 
 namespace SelfHosted
 {    
@@ -24,10 +25,15 @@ namespace SelfHosted
             }
             MultipartFormDataStreamProvider streamProvider = new MultipartFormDataStreamProvider(ServerUploadFolder);
             await Request.Content.ReadAsMultipartAsync(streamProvider);
-
+            foreach (MultipartFileData data in streamProvider.FileData)
+            {
+                string strFileName = data.Headers.ContentDisposition.FileName.Trim('"');
+                File.Move(data.LocalFileName, ServerUploadFolder+"\\"+strFileName);                
+            }
+            //Collection<MultipartFileData> data = streamProvider.FileData;
             return new FileResult
             {
-                FileNames = streamProvider.FileData.Select(entry => entry.LocalFileName),
+                FileNames = streamProvider.FileData.Select(entry => entry.LocalFileName),                
                 Submitter = streamProvider.FormData["submitter"]
             };
 
